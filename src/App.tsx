54ca213bd96f09cc453a4948c9ff512084e0c8e4
ObuchiYuki <yukibochi1@gmail.com>
2023-03-07 13:48:23 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,6 @@ export default function App() {
 
   useEffect(() => {
     const ydoc = new Y.Doc()
-    
     const provider = new WebSocketProvider({
       serverUrl: "ws://y-websocket-server.eastasia.azurecontainer.io", roomname: "sample.document", doc: ydoc
     })
